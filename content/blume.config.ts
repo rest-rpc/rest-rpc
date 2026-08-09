37321a5e0865da8a1a39fb4959ab3e88bb93cdb9
Simo <simo.alanne@tuni.fi>
2026-08-09 23:44:53 +0300

    chore: format content/ files

--- content/blume.config.ts
+++ content/blume.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from "blume";
 
 export default defineConfig({
-  title: "rest-rpc",
-  description: "REST-shaped APIs with function-shaped TypeScript",
-  github: {
-    owner: "rest-rpc",
-    repo: "rest-rpc",
-  },
+	title: "rest-rpc",
+	description: "REST-shaped APIs with function-shaped TypeScript",
+	github: {
+		owner: "rest-rpc",
+		repo: "rest-rpc",
+	},
 });
