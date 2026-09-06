56626fa0cbf17864ab98676903db1b95ec9f1eb2
Simo <simo.alanne@tuni.fi>
2026-09-06 15:27:09 +0300

    docs: document Fetch and Node runtime handlers

--- content/docs/server/meta.ts
+++ content/docs/server/meta.ts
@@ -1,6 +1,6 @@
 import { defineMeta } from "blume";
 
 export default defineMeta({
-	pages: ["express", "fastify", "hono", "nest", "fetch"],
+	pages: ["express", "fastify", "hono", "nest", "fetch", "node"],
 	title: "Server",
 });
